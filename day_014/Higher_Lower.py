52475d57a8dc2744e62ca19f8b4afdf057ec7a24
flighthman69 <flightman69420@gmail.com>
2023-01-22 20:06:36 +0530

    added comments for day 14

--- day_014/Higher_Lower.py
+++ day_014/Higher_Lower.py
@@ -4,11 +4,15 @@ from art import logo,vs,lost
 from os import system, name
 
 score = 0
+# Function to clear the screen
 def clear():
     if name == 'nt':
         system('cls')
     else:
         system('clear')
+
+# function that checks the user input
+
 def Higher_Lower(u1_name,u1_fls,u2_name,u2_fls):
     clear()
     print(logo)
@@ -17,43 +21,60 @@ def Higher_Lower(u1_name,u1_fls,u2_name,u2_fls):
     print(f"Instagram User: {u1_name}")
     print(vs)
     print(f"Instagram User: {u2_name}\n")
+    
+    # Checking if the user is right
     isHigher = u1_fls > u2_fls
     isLower = u1_fls < u2_fls
 
     userInput = input(f"Does {u1_name} has [h]igher or [l]ower followrs than {u2_name}: ").lower()
 
     if (userInput == 'h' and isHigher) or (userInput == 'l' and isLower):
-        return 0
+        return 0 #If the user is right returning 0
     else:
-        return 2
+        return 2 #If the user is wrong returning 2
 
 def main():
+    
+    #Choosing User 1
     user = random.choice(data)
     user1_name = user["name"]
     user1_followers = user["follower_count"]
-    data.remove(user)
+    
+
+    #Choosing User 2 
     user = random.choice(data)
+    while user["name"] == u1_name: #making sure it chooses two different users
+        user = random.choice(data)
     user2_name = user["name"]
     user2_followers = user["follower_count"]
-    data.remove(user)
+    
     global score
+    # Calling the Function
     status = Higher_Lower(user1_name,user1_followers,user2_name,user2_followers)
+    
+    # Running the program as long as the player wins
     while status == 0:
         score +=1
         user1_followers = user2_followers
         user1_name = user2_name 
+
         user = random.choice(data)
+        while user["name"] == u1_name: #making sure it chooses two different users
+            user = random.choice(data)
+
         user2_name = user["name"]
         user2_followers = user["follower_count"]
         data.remove(user)
         
         status = Higher_Lower(user1_name,user1_followers,user2_name,user2_followers)
+
+    #Ending the game if the player loose
     if status != 0:
         clear()
         print(lost,'\n')
         print(f"Your final score: {score}")
         play_again = input(f"Oops you lost. Wanna play again [y/n]: ").lower()
-        if play_again == 'y':
+        if play_again == 'y': # Asking the player if they want to play again
             score = 0
             main()
         else:
