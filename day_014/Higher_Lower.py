3c1d4841b5ebeeb9ddfd4dc747cefa50377c5252
flighthman69 <flightman69420@gmail.com>
2023-01-24 11:35:28 +0530

    Day 15 coffee maker

--- day_014/Higher_Lower.py
+++ day_014/Higher_Lower.py
@@ -1,9 +1,11 @@
 from data import data
 import random
-from art import logo,vs,lost
+from art import logo, vs, lost
 from os import system, name
 
 score = 0
+
+
 # Function to clear the screen
 def clear():
     if name == 'nt':
@@ -11,9 +13,10 @@ def clear():
     else:
         system('clear')
 
+
 # function that checks the user input
 
-def Higher_Lower(u1_name,u1_fls,u2_name,u2_fls):
+def higher_lower(u1_name, u1_fls, u2_name, u2_fls):
     clear()
     print(logo)
     print(f"Your Score: {score}")
@@ -21,7 +24,7 @@ def Higher_Lower(u1_name,u1_fls,u2_name,u2_fls):
     print(f"Instagram User: {u1_name}")
     print(vs)
     print(f"Instagram User: {u2_name}\n")
-    
+
     # Checking if the user is right
     isHigher = u1_fls > u2_fls
     isLower = u1_fls < u2_fls
@@ -29,56 +32,56 @@ def Higher_Lower(u1_name,u1_fls,u2_name,u2_fls):
     userInput = input(f"Does {u1_name} has [h]igher or [l]ower followrs than {u2_name}: ").lower()
 
     if (userInput == 'h' and isHigher) or (userInput == 'l' and isLower):
-        return 0 #If the user is right returning 0
+        return 0  # If the user is right returning 0
     else:
-        return 2 #If the user is wrong returning 2
+        return 2  # If the user is wrong returning 2
+
 
 def main():
-    
-    #Choosing User 1
+    # Choosing User 1
     user = random.choice(data)
     user1_name = user["name"]
     user1_followers = user["follower_count"]
-    
 
-    #Choosing User 2 
+    # Choosing User 2
     user = random.choice(data)
-    while user["name"] == user1_name: #making sure it chooses two different users
+    while user["name"] == user1_name:  # making sure it chooses two different users
         user = random.choice(data)
     user2_name = user["name"]
     user2_followers = user["follower_count"]
-    
+
     global score
     # Calling the Function
-    status = Higher_Lower(user1_name,user1_followers,user2_name,user2_followers)
-    
+    status = higher_lower(user1_name, user1_followers, user2_name, user2_followers)
+
     # Running the program as long as the player wins
     while status == 0:
-        score +=1
+        score += 1
         user1_followers = user2_followers
-        user1_name = user2_name 
+        user1_name = user2_name
 
         user = random.choice(data)
-        while user["name"] == user1_name: #making sure it chooses two different users
+        while user["name"] == user1_name:  # making sure it chooses two different users
             user = random.choice(data)
 
         user2_name = user["name"]
         user2_followers = user["follower_count"]
         data.remove(user)
-        
-        status = Higher_Lower(user1_name,user1_followers,user2_name,user2_followers)
 
-    #Ending the game if the player loose
+        status = higher_lower(user1_name, user1_followers, user2_name, user2_followers)
+
+    # Ending the game if the player loose
     if status != 0:
         clear()
-        print(lost,'\n')
+        print(lost, '\n')
         print(f"Your final score: {score}")
         play_again = input(f"Oops you lost. Wanna play again [y/n]: ").lower()
-        if play_again == 'y': # Asking the player if they want to play again
+        if play_again == 'y':  # Asking the player if they want to play again
             score = 0
             main()
         else:
             print("\nThanks for playing, have a good day :)")
             return 0
+
 
 main()
---
+++ day_015/Coffe_Maker.py
@@ -0,0 +1,134 @@
+from os import name, system
+from art import logo
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+        "water":500,
+        "milk":500,
+        "coffee":120,
+        }
+
+profit = 0
+total_coffes_made = {"espresso": 0, "latte": 0, "cappuccino": 0}
+
+def clear():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+def check_availability(coffee):
+    ingre = MENU[coffee]["ingredients"]
+
+    item_left_value = []
+    for item in ingre:
+
+        item_left = resources[item] // ingre[item]
+        item_left_value.append(item_left)
+
+    return min(item_left_value)
+def report():
+    print("\nResource Left: ")
+    for res in resources:
+        print(f"{res}: {resources[res]}")
+    print()
+
+    can_make = {}
+    for coffee in MENU:
+        can_make[coffee] = check_availability(coffee)
+    print("This resources is enough to make: ")
+    for _ in can_make:
+        print(f"{_}: {can_make[_]} (OR)")
+    print()
+
+    global profit
+    print(f"Total profit made: ${profit}")
+    print(f"Total coffees made: {total_coffes_made}")
+
+
+def calc_payment():
+    print(f"Please pay your bill in coins ${coffee_cost}")
+
+    coins_value = {"penny": 0.01,"dime": 0.05,"cent": 0.10, "quater": 0.25}
+    tot = 0
+    for coin in coins_value:
+        tot += int(input(f"{coin}: ")) * coins_value[coin]
+    return tot
+
+
+def make_coffee(coffee,coffee_cost,coffee_ingredients):
+    is_available = check_availability(coffee)
+    if is_available == 0:
+        return 1
+    payment = calc_payment()     
+    if payment < coffee_cost:
+        return payment
+    elif payment > coffee_cost:
+        balance = payment - coffee_cost
+        print(f"Thank you for your payment ${payment:.2f} here's your balance: ${balance:.2f}")
+        
+        ingre = MENU[coffee]["ingredients"]
+        item_left_value = []
+        for item in ingre:
+            resources[item] -= ingre[item]
+        global profit
+        profit += coffee_cost
+
+        total_coffes_made[coffee] += 1
+        return 0 
+
+
+while True:
+    
+    clear()
+    print(logo)
+    print()
+    user_input = input("Choose your energy cup (espresso/latte/cappuccino): ").lower()
+    
+    if user_input == 'report':
+        report()
+        input("Press Enter to continue: ")
+    elif user_input == 'exit':
+        print("Have a great day ☕")
+        break
+    else:
+        coffee = user_input
+        coffee_cost = MENU[user_input]["cost"]
+        coffee_ingredients = MENU[user_input]["ingredients"]
+
+        status = make_coffee(coffee,coffee_cost,coffee_ingredients)
+        if status == 1:
+            print(f"Insufficient resources to make {coffee}\n")
+        elif  status == 0:
+            print(f"Here's your {coffee} ☕. Enjoy it and have a great day\n")
+        else:
+            print(f"The amount ${status} is not enough.\n")
+        input("Press Enter to continue")
+
+
+
+
+    
